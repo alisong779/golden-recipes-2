1093c907d0bf310cde4542a4484a9349627c56df
Alison Golden <alisong779@yahoo.com>
2019-07-12 23:32:54 -0400

    add omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {registrations: 'registrations'}
   root "static_pages#home"
+  devise_for :users, :controllers => {registrations: 'registrations'}
+
+    devise_scope :user do
+      get 'login', to: 'devise/sessions#new'
+    end
+
+    devise_scope :user do
+      get 'signup', to: 'devise/registrations#new'
+    end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_12_195600) do
+ActiveRecord::Schema.define(version: 2019_07_13_024651) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2019_07_12_195600) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
