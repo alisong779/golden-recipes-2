c92df0af194e961fdf9d51a1fc755ae0433acfeb
Alison Golden <alisong779@yahoo.com>
2019-08-19 23:27:35 -0400

    add js for comments

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,63 +1,80 @@
 const BASE_URL = 'http://localhost:3000'
 
 function displayCreateForm(){
-	document.querySelector('#comment-form').innerHTML = `
-	<form onsubmit="createComment(); return false;">
-	<label>Comment:</label>
-	<input type="textarea" id="comments">
-	<input type="submit" value="Create Comment"
-	</form>
-	`
+	let commentFormDiv = document.getElementById('comment-form')
+	let html = `
+		<form onsubmit="createComment(); return false;">
+		<label>Comment:</label>
+		<input type="textarea" id="comment">
+		<input type="submit" value="Create Comment"
+		</form>
+		`
+	commentFormDiv.innerHTML = html;
 }
+
+function getComments(){
+	clearForm()
+	let main = document.getElementById('main')
+	main.innerHTML = '<ul>'
+	fetch(BASE_URL + '/comments')
+	.then(resp => resp.json())
+	.then(comments => {
+		main.innerHTML += comments.map(comment => {
+		const cmt = new Ct(comment)
+		return cmt.renderComment()
+	}).join('')
+	main.innerHTML += '</ul>'
+	})
+}
+
+function	displayComment(e){
+	e.preventDefault();
+	clearForm()
+	let id = this.dataset.id
+	let main = document.getElementById('main')
+	main.innerHTML = ''
+
+	fetch(BASE_URL + '/comments/' + id)
+		.then(resp => resp.json())
+		.then(comment =>{
+			main.innerHTML += '<h3>${comment.comment}</h3>'
+		})
+}
+
 
 function createComment(){
 	const comt = {
-		comment: document.getElementById("comment").value
+		comment: document.getElementById('comment').value
 	}
-	fetch(BASE_URL + '/comments/', {
-		method: "POST",
-		body: JSON.stringify({comt}),
+	fetch(BASE_URL + '/comments', {
+		method: 'POST',
+		body: JSON.stringify({ comt }),
 		headers: {
 			'Content-Type': 'application/json',
 			'Accept': 'application/json'
 		}
 	}).then(resp => resp.json())
 	.then(comt => {
-		alert("Comment Sucessfully Submitted!");
-		let cmt = new Comment(comt);
-		let newCmt = document.querySelector("#comment-form");
-		display.innerHTML += `
-		<ul>
-			<li>Comment: ${comt.comment}</li>
-		</ul>
-		`
-		displayComments();
-	});
-}
-
-
-function displayComments(){
-	fetch(BASE_URL + '/comments/all')
-	.then(resp => resp.json())
-	.then(comments => {
-		comments.map(comment => {
-			index.innerHTML+= `
-			<h4>${comment.comment}</h4>
-			`
-		})
+		alert("Comment Sucessfully Submitted!")
+		let cmt = new Ct(comt);
+		document.querySelector("#main ul").innerHTML += cmt.renderComment()
+		let commentFormDiv = document.getElementById('comment-form')
+		commentFormDiv.innerHTML = ''
 	})
 }
 
-class Comment{
+function clearForm(){
+	let commentFormDiv = document.getElementById('comment-form')
+	commentFormDiv.innerHTML = ''
+}
+
+
+class Ct{
 	constructor(comment){
 		this.id = comment.id
 		this.comment = comment.comment
 	}
-	renderComments(){
-		return `
-			<div>
-			<p>Comments: ${this.comment}</p>
-			</div>
-		`
+	renderComment(){
+		return `<li><a href="#" data-id="${this.id}">${this.comment}</a></li>`
 	}
 }
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,23 +2,21 @@ class CommentsController < ApplicationController
 	before_action :set_comment, only: [:show, :edit, :update]
 
 	def index
-		@comments = Comment.all
-		respond_to do |format|
-			format.html
-			format.json {render json: @comments}
+		comments = Comment.all
+		render json: comments
 	end
 
 	def show
-		@comment = Comment.find_by_id(params[:id])
-		render json: @comment
+		comment = Comment.find_by_id(params[:id])
+		render json: comment
 	end
 
 	def create
-		@comment = Comment.new(comment_params)
-		if @comment.save
-    	render json: @comment, status: 201
+		comment = Comment.new(comment_params)
+		if comment.save
+    	render json: comment, status: 201
 		else
-			render json: {errors: @comment.errors.full_messages}, status: :bad_request
+			render json: {errors: comment.errors.full_messages}, status: :bad_request
 		end
 	end
 
