c55f85025a08e12ee08f6b3dfcba1f204e5f6dea
Alison Golden <alisong779@yahoo.com>
2019-08-21 16:00:49 -0400

    revise js

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -25,7 +25,7 @@ function displayCreateForm(id){
 
 function createComment(id){
 	comt = {
-		comment: document.getElementById("comment").value
+		comments: document.getElementById("comment").value
 	}
 
 	fetch(BASE_URL + '/recipes/${id}/comments', {
@@ -45,32 +45,44 @@ function createComment(id){
 
 
 function getComments(id){
-	let main = document.getElementById('main')
-	main.innerHTML = '<ul>'
-	fetch(BASE_URL + '/recipes/${id}/comments')
+	$("#main").html(`<ul>`)
+	// let main = document.getElementById("main")
+	// main.innerHTML = `<ul>`
+	fetch(BASE_URL + `/recipes/${id}.json`)
 	.then(resp => resp.json())
 	.then(comments => {
-		main.innerHTML += comments.map(comment => {
+		document.getElementById("main").innerHTML += recipe.comments.map(comment => {
 		let cmt = new Ct(comment)
-		return cmt.renderComment(comment)
+		return cmt.renderCommentLink()
 	}).join('')
-	main.innerHTML += '</ul>'
+	$("#main").append(`<ul>`)
+	addListenersToLinks()
 	})
 }
 
-function	displayComment(e){
-	e.preventDefault();
-	clearForm()
-	let id = this.dataset.id
-	let main = document.getElementById('main')
-	main.innerHTML = ''
-
-	fetch(BASE_URL + '/recipes/${this.dataset.recipeid}/comments/${this.dataset.id}')
-		.then(resp => resp.json())
-		.then(comment =>{
-			main.innerHTML += '<h3>${comment.comment}</h3>'
-		})
+function addListenersToLinks() {
+    document.querySelectorAll("#comments-links").forEach(function(link) {
+        link.addEventListener("click", displayComment)
+    })
 }
+
+// function	displayComment(e){
+// 	e.preventDefault();
+// 	// let id = this.dataset.id
+// 	// let main = document.getElementById('main')
+// 	// main.innerHTML = ''
+//
+// 	fetch(BASE_URL + `/recipes/${this.dataset.recipeid}/comments/${this.dataset.id}`)
+// 		.then(resp => resp.json())
+// 		.then(comment =>{
+// 			let cmt = new Ct(comment)
+// 			document.querySelectorAll("#main-lis").forEach(function(li)){
+// 				if(parseInt(li.dataset.id) === cmt.id){
+// 					li.innerHTML += cmt.renderComment()
+// 				}
+// 			})
+// 		})
+// }
 
 
 function clearForm(){
@@ -85,7 +97,7 @@ class Ct{
 		this.comment = comment.comment
 		this.recipe_id = comment.recipe_id
 	}
-	renderComment(){
+	renderCommentLink(){
 		return `<li id="comments-lis" data-id="${this.id}"><a href="#" data-id="${this.id}" data-recipeid="${this.recipe_id}" id="comments-links">${this.comment}</a></li>`
 	}
 }
